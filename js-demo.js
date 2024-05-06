659fbe1223bf67717059860547280f90afafcc9d
Derektoh <165088870+Derektoh@users.noreply.github.com>
2024-05-06 00:38:49 -0700

    Create js-demo.js

---
+++ js-demo.js
@@ -0,0 +1,7 @@
+  JS Activity Clock
+   Name: Derrick Francisco
+   Date: 04/29/2024  
+
+   function getWeekday(dayNum)
+      Returns the text of the day of the week where dayNum
+      is the number of the week from 0 (Sunday) to 6 (Saturday)
